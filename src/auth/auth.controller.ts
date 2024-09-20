936c16934efc57a3dc33f9613c6769f09759be00
jaavispeed <ja.rodriguezc@duocuc.cl>
2024-09-20 17:44:17 -0300

    Logica auth y products funcionando

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -25,6 +25,15 @@ export class AuthController {
     return this.authService.login(loginUserDto);
   }
   
+
+  @Get('check-status')
+  @Auth()
+  checkAuthStatus(
+    @GetUser() user: User
+  ){
+    return this.authService.checkAuthStatus(user);
+  }
+  
   @Get('private')
   @Auth()
   PrivateRoute(
