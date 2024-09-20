eda2d3240acfab61ea59de876db300c97ff189b7
jaavispeed <ja.rodriguezc@duocuc.cl>
2024-09-19 21:28:00 -0300

    Implementacion de cors

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -3,7 +3,6 @@ import { IsEmail, IsString, MaxLength, MinLength } from "class-validator";
 
 export class LoginUserDto{
     
-    @IsString()
     @IsEmail()
     email: string;
     
--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,21 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Configuración global de CORS
+  app.enableCors({
+    origin: 'http://localhost:4200', // Permite solicitudes desde este origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
+  // Configuración global del prefijo de rutas
   app.setGlobalPrefix('api');
 
-  app.useGlobalPipes( 
-    new ValidationPipe({ 
-      whitelist: true, 
-      forbidNonWhitelisted: true, 
+  // Configuración de ValidationPipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
     })
   );
 
