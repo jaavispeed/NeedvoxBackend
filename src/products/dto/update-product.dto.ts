940a1b73555b6a2732daabf09905d85828ebea2e
jaavispeed <ja.rodriguezc@duocuc.cl>
2024-09-22 23:17:29 -0300

    actualizacion de update

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,20 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
+import { IsInt, IsNumber, IsOptional, IsPositive, IsString, Min } from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+
+    @IsString()
+    title: string;
+  
+    @IsNumber()
+    @IsOptional()
+    @Min(0)
+    price?: number;
+  
+    @IsInt()
+    @IsOptional()
+    @Min(0)
+    stock?: number;
+
+}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -14,19 +14,20 @@ export class ProductsController {
   @Post()
   create
   (@Body() createProductDto: CreateProductDto,
-  @GetUser() user: User,
-) {
+  @GetUser() user: User,) 
+  {
     return this.productsService.create(createProductDto, user );
   }
 
   @Get()
-  findAll(@Query() paginationDto:PaginationDto) {
-    
+  findAll(@Query() paginationDto:PaginationDto) 
+  {
     return this.productsService.findAll(paginationDto);
   }
 
   @Get(':term')
-  findOne(@Param('term') term: string) {
+  findOne(@Param('term') term: string) 
+  {
     return this.productsService.findOne(term);
   }
 
@@ -34,12 +35,14 @@ export class ProductsController {
   update(
     @Param('id', ParseUUIDPipe) id: string, 
     @Body() updateProductDto: UpdateProductDto,
-    @GetUser() user: User) {
+    @GetUser() user: User) 
+    {
     return this.productsService.update(id, updateProductDto, user);
   }
 
   @Delete(':id')
-  remove(@Param('id', ParseUUIDPipe) id: string) {
+  remove(@Param('id', ParseUUIDPipe) id: string) 
+  {
     return this.productsService.remove(id);
   }
 }
