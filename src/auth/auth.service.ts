eaa923683b940a11ffe70860f5efbe85634ffd88
CBA Vasquez <seb.vasquez@duocuc.cl>
2024-11-04 14:26:00 -0300

    activar/desactivar usuario/ vista admin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,13 +2,10 @@ import { BadRequestException, Injectable, InternalServerErrorException, Unauthor
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
-
 import * as bcrypt from 'bcrypt';
-
 import { User } from './entities/user.entity';
 import { LoginUserDto, CreateUserDto } from './dto';
 import { JwtPayload } from './interfaces/jwt.payload.interface';
-
 
 @Injectable()
 export class AuthService {
@@ -17,64 +14,66 @@ export class AuthService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
     private readonly jwtService: JwtService
-  ){}
-
+  ) {}
 
   async create(createUserDto: CreateUserDto) {
-    try{
-      const {password, ...userData} = createUserDto;
-
+    try {
+      const { password, ...userData } = createUserDto;
       const user = this.userRepository.create({
         ...userData,
         password: bcrypt.hashSync(password, 10)
       });
-      await this.userRepository.save(user)
+      await this.userRepository.save(user);
       delete user.password;
 
       return {
         ...user,
-      token: this.getJwtToken({id: user.id})
-    };
-    }catch(error){
+        token: this.getJwtToken({ id: user.id })
+      };
+    } catch (error) {
       this.handleDBErrors(error);
     }
   }
 
-  async login(loginUserDto: LoginUserDto){
-    const{password, email} = loginUserDto;
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto;
 
     const user = await this.userRepository.findOne({
-      where: {email},
-      select: {email: true, password: true, id: true}
+      where: { email },
+      select: { email: true, password: true, id: true, isActive: true } // Incluir isActive
     });
 
-    if(!user)
+    if (!user) {
       throw new UnauthorizedException('Credentials are not valid (email)');
-    if(!bcrypt.compareSync(password, user.password))
-      throw new UnauthorizedException('Credentials are not valid (email)');
-    return {
-      ...user,
-    token: this.getJwtToken({id: user.id})
-  };
-  }
+    }
+    if (!user.isActive) { // Verifica si el usuario está activo
+      throw new UnauthorizedException('User account is inactive'); // Lanza excepción si está inactivo
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Credentials are not valid (password)');
+    }
 
-  async checkAuthStatus(user:User){
     return {
       ...user,
-    token: this.getJwtToken({id: user.id})
+      token: this.getJwtToken({ id: user.id }),
     };
   }
 
-  private getJwtToken(payload: JwtPayload){
+  async checkAuthStatus(user: User) {
+    return {
+      ...user,
+      token: this.getJwtToken({ id: user.id })
+    };
+  }
 
-    const token = this.jwtService.sign(payload );
+  private getJwtToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
     return token;
   }
 
   private handleDBErrors(error: any): never {
-    if(error.code === '23505')throw new BadRequestException(error.detail);
-    console.log(error)
-
+    if (error.code === '23505') throw new BadRequestException(error.detail);
+    console.log(error);
     throw new InternalServerErrorException('Please check server logs');
   }
 }
