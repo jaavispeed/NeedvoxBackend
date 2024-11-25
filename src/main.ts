d64bc5fe2b694fc35b93333f754111f595674c36
jaavispeed <ja.rodriguezc@duocuc.cl>
2024-11-25 00:12:50 -0300

    adaptado el serverless

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,43 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as serverless from 'serverless-http';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  try {
+    const app = await NestFactory.create(AppModule);
 
-  // Configuración global de CORS
-  app.enableCors({
-    origin: '*', // Permite solicitudes desde este origen
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Métodos permitidos
-    credentials: true,
-  allowedHeaders:
-      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
-  });
+    // Configuración global de CORS
+    app.enableCors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+      allowedHeaders:
+        'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+    });
 
-  // Configuración global del prefijo de rutas
-  app.setGlobalPrefix('api');
+    // Configuración global del prefijo de rutas
+    app.setGlobalPrefix('api');
 
-  // Configuración de ValidationPipe
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    })
-  );
+    // Configuración de ValidationPipe
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      })
+    );
 
-  await app.listen(3000);
+    // Convertimos la app NestJS a una función serverless
+    const handler = serverless(app.getHttpAdapter().getInstance());
+
+    // Exponemos la función handler para Vercel
+    return handler;
+
+  } catch (error) {
+    console.error('Error en la inicialización de NestJS:', error);
+    throw error;
+  }
 }
-bootstrap();
+
+// Ejecutamos la aplicación como serverless
+export const handler = bootstrap();
