6f7cae49d6b45f1f0f871da28ea652ca3d56b55b
jaavispeed <ja.rodriguezc@duocuc.cl>
2024-11-25 00:19:52 -0300

    ruta nueva

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,3 +1,4 @@
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import {
   Controller,
   Get,
@@ -15,7 +16,7 @@ import {
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PaginationDto } from 'src/common/dtos/pagination.dto';
+
 import { Auth, GetUser } from 'src/auth/decorators';
 import { User } from '../auth/entities/user.entity';
 import { Lote } from 'src/lotes/entities/lotes.entity';
