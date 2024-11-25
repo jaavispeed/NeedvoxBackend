7cd5689b2f1a35c2185257f12519e100ceb86065
Javier Rodriguez <101828419+jaavispeed@users.noreply.github.com>
2024-11-25 00:29:17 -0300

    Update products.controller.ts

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -17,10 +17,7 @@ import {
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-<<<<<<< HEAD
-=======
-import { PaginationDto } from 'src/common/dtos/pagination.dto';
->>>>>>> 09e092d1d94a08d22574dc932bd6b64109d3211c
+import { PaginationDto } from '../../common/dtos/pagination.dto';
 import { Auth, GetUser } from 'src/auth/decorators';
 import { User } from '../auth/entities/user.entity';
 import { Lote } from 'src/lotes/entities/lotes.entity';
