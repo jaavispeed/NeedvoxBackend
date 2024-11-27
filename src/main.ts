00c0d3c40db1e134b732e18a65aa0b5b424041db
Javier Rodriguez <101828419+jaavispeed@users.noreply.github.com>
2024-11-26 23:11:40 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   // Configuración global de CORS
 app.enableCors({
-  origin: '*',  // Cambia esto por el dominio de tu frontend
+  origin: 'https://needvox.vercel.app/',  // Cambia esto por el dominio de tu frontend
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],  // Métodos permitidos
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-Control-Allow-Credentials, Authorization',
   credentials: true,  // Habilita el envío de cookies si es necesario
