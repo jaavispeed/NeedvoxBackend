b2a138549afc0cfa5d56775dcd74a0f9b804f468
Javier Rodriguez <101828419+jaavispeed@users.noreply.github.com>
2024-12-05 16:15:27 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    allowedHeaders: [
-      'Origin',
-      'X-Requested-With',
-      'Content-Type',
-      'Accept',
-      'Authentication',
-      'Access-Control-Allow-Credentials',
-      'Authorization',
-    ],
+     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-Control-Allow-Credentials, Authorization',
     credentials: true,
   });
   
